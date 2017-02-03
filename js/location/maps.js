8cd0ebb328176e36e1df290dd16d0df0857d9a26
Christian2804 <c.navolskyi@gmail.com>
2017-02-03 19:19:30 +0100

    Finished javascript for maps

---
+++ js/location/maps.js
@@ -0,0 +1,39 @@
+function loadMap() {
+  $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAN4cGaQXGLbdHW7mFFzMXDn4brOFfS-AY&signed_in=true", initMap);
+}
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Sie sind hier.');
+      map.setCenter(pos);
+
+      document.getElementById("text").innerHTML = "Wie sie sehen können wird Ihnen hier Ihre aktuelle Position angezeigt."
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
