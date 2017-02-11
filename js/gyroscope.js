007af6cd368a0c59783b9c9876b9c66e5cb09b69
Christian2804 <c.navolskyi@gmail.com>
2017-02-11 23:58:35 +0100

    Removed unnecessary sensors

--- js/gyroscope.js
+++
@@ -1,8 +0,0 @@
-window.ondevicemotion = function(event) {
-	alpha = event.rotationRate.alpha * Math.PI / 180;
-
-  var text = document.getElementById("longText");
-
-  text.style.transform = "rotate(" + alpha + "deg)";
-
-}
